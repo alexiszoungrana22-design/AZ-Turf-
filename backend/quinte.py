8fd36009763083efe289f366817415d862159970
alexiszoungrana22-design <alexis.zoungrana22@gmail.com>
2026-07-18 22:58:45 +0000

    Add favorite and max index to ticket generation

--- backend/quinte.py
+++ backend/quinte.py
@@ -1,10 +1,19 @@
 from backend.ranking import classer_chevaux
 
 def generer_ticket(chevaux):
+
     classement = classer_chevaux(chevaux)
 
     return {
+
         "bases": classement[:2],
+
         "chances": classement[2:5],
-        "outsiders": classement[5:7]
+
+        "outsiders": classement[5:7],
+
+        "favori": classement[0],
+
+        "indice_max": classement[0]["indiceAZ"]
+
     }
